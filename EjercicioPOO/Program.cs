c8184b9a707d2b94d6c74171c93e25e7b8a668e6
caput <caput@LUCIANO>
2023-02-22 18:41:32 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioPOO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EjercicioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            realiarTarea();
+        }
+
+        static void realiarTarea()
+        {
+            Punto origen = new Punto();
+            Punto destino = new Punto(130,40);
+            double distancia = origen.CacularDistancia(destino);
+            Console.WriteLine($"La distancia entre los puntos es: {distancia}"); 
+        }
+        
+    }
+
+
+}
---
+++ EjercicioPOO/Punto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPOO
+{
+    internal class Punto
+    {  
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y; 
+            
+            //Console.WriteLine($"Coordenada X: {x}; Coordenada Y: {y}");
+        }
+        public Punto()
+        {
+            this.x = 0;
+            this.y = 0;
+
+            //Console.WriteLine("Este es el constructor por defecto");
+        }
+        public double CacularDistancia(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+            int yDif = this.y - otroPunto.y;
+            double distancia = Math.Sqrt(Math.Pow(xDif, 2)+Math.Pow(yDif, 2));
+            return distancia;
+        }
+
+        private int x, y;
+
+
+      
+       
+    }
+}
